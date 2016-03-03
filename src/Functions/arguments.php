8bac46931413b627fef70fff3742455ebab8a159
Wilk <vincenzo@ibuildings.it>
2016-03-03 19:29:08 +0100

    studied functions

--- src/Functions/arguments.php
+++ src/Functions/arguments.php
@@ -25,3 +25,69 @@
     }
 
     default_extra_params("test");
+
+    // typed arguments
+    function typed_args(array $names) {
+        echo count($names).PHP_EOL;
+    }
+    //typed_args("hello"); // throws a fatal error
+    typed_args([10,20,30]);
+
+    class A {}
+    class B extends A {}
+    class C {}
+
+    function class_typed_args(A $a) {
+        echo get_class($a) . PHP_EOL;
+    }
+    class_typed_args(new A());
+    class_typed_args(new B());
+    //class_typed_args(new C()); // throws a fatal error
+
+    interface I {}
+    class IA implements I {}
+    class IB extends IA {}
+    class IC extends IB {}
+
+    function interface_typed_args(I $i) {
+        echo get_class($i) . PHP_EOL;
+    }
+    interface_typed_args(new IA());
+    interface_typed_args(new IB());
+    interface_typed_args(new IC());
+
+    function nullable_typed_args(I $i = null) {
+        echo get_class($i) . PHP_EOL;
+    }
+
+    nullable_typed_args(new IA());
+    nullable_typed_args(); // throws a warning
+
+    // weak typing (only available on PHP 7)
+    /*function weak_coerced_typed_args(int $a, int $b) {
+        return $a + $b;
+    }
+
+    echo weak_coerced_typed_args(10, 20).PHP_EOL;
+    echo weak_coerced_typed_args(10.40, 10.50).PHP_EOL;*/
+
+    // variable-length args list (only available on PHP 5.6)
+    /*function length_typed_args(...$args) {
+        foreach ($args as $arg) echo $arg.PHP_EOL;
+    }
+    length_typed_args(...[10, 20, 30]);
+
+    function length_hint_typed_args(string ...$args) {
+        foreach ($args as $arg) echo $arg.PHP_EOL;
+    }
+
+    length_hint_typed_args("hello", "cruel", "world");*/
+
+    // arguments functions for PHP older versions
+    function use_args_func() {
+        $count = func_num_args();
+        echo "$count arguments".PHP_EOL;
+        $args = func_get_args();
+        foreach ($args as $arg) echo $arg.PHP_EOL;
+    }
+    use_args_func("hello", "cruel", "world");
