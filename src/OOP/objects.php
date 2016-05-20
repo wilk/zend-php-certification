4b03f565e7aee660d2f028dfe17353399a81bf1c
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-05-20 08:05:30 +0200

    improved autoloding example

--- src/OOP/objects.php
+++ src/OOP/objects.php
@@ -2,6 +2,7 @@
 require_once "classes/Person.php";
 require_once "classes/Skill.php";
 
+// cloning
 $skill = new Skill("PHP");
 $foo = new Person("foo");
 $foo->setSkill($skill);
@@ -14,3 +15,8 @@ echo $foo->name.PHP_EOL;
 echo $foo->getSkill()->name.PHP_EOL;
 echo $bar->name.PHP_EOL;
 echo $bar->getSkill()->name.PHP_EOL;
+
+// serializing/unserializing
+
+$str = serialize($foo);
+var_dump($str);
