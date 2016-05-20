bc71a91cfc2f9f7f11c27fea9efb220e8cf738bc
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-05-20 08:01:55 +0200

    improved autoloding example

--- src/OOP/autoload.php
+++ src/OOP/autoload.php
@@ -1,13 +1,19 @@
 <?php
-spl_autoload_register(function ($class) {
-    echo $class.PHP_EOL;
-    require_once "$class.php"; // trying to load the class being instantiated
+
+set_error_handler(function ($err, $type) {
+    var_dump($err);
+    var_dump($type);
 });
 
-//$foo = new Person; // it will throw an error
+// this function is called when PHP cannot find a class
+// if the class does not exist in folder classes/ than a fatal error is thrown
+spl_autoload_register(function ($class) {
+    echo $class.PHP_EOL;
+    require_once "classes/$class.php"; // trying to load the class being instantiated
+});
 
-class Person {
-    public function __autoload() {
+// Person does not exist in the current file and the person.php is not included 'til is loaded
+$foo = new Person("foo");
+echo $foo->name.PHP_EOL;
 
-    }
-}
+$bar = new Superman(); // this will throw an error
