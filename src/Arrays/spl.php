e94eec0001cc3767e4cd6110136bbcdf71c0884b
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-05-20 07:39:35 +0200

    improved array spl

--- src/Arrays/spl.php
+++ src/Arrays/spl.php
@@ -10,7 +10,7 @@ $obj->asort();
 
 foreach ($obj as $key => $value) echo "$key ... $value".PHP_EOL;
 
-$obj->sort();
+$obj->ksort();
 
 foreach ($obj as $key => $value) echo "$key ... $value".PHP_EOL;
 
@@ -20,3 +20,11 @@ Flag Effect
 ArrayObject::STD_PROP_LIST  Properties of the object have their normal functionality when accessed as list (var_dump, foreach, etc.).
 ArrayObject::ARRAY_AS_PROPS Entries can be accessed as properties (read and write).
 */
+
+var_dump($obj->dollar);
+$obj->setFlags(ArrayObject::ARRAY_AS_PROPS);
+var_dump($obj->dollar);
+
+foreach ($obj as $key => $value) {
+    echo "$key => $value".PHP_EOL;
+}
