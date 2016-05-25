2aaecd5a2114a8895abc068d9e1fc93da67e155d
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-05-25 08:20:28 +0200

    improved security configurations

--- src/Security/configuration.php
+++ src/Security/configuration.php
@@ -15,3 +15,6 @@ ini_set('display_errors', false); // avoid to display errors on stdout
 ini_set('log_errors', true); // log them instead
 
 // Disabling functions and classes
+ini_set('disable_functions', 'exec,passthru,shell_exec,system');
+ini_set('disable_classes', 'foo');
+ini_set('open_basedir', '/tmp'); // for include and fopen
