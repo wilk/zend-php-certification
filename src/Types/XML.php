a259331430d43c63e71baec0760817c5c98dec89
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-05-27 08:24:45 +0200

    improved xml

--- src/Types/XML.php
+++ src/Types/XML.php
@@ -1,4 +1,5 @@
 <?php
+// PHP XML parser uses LibXML and Expat libraries and they're enabled by default
 
 // stylesheet for XML Doc
 $xslDoc = new DOMDocument();
@@ -12,3 +13,23 @@ $xmlDoc->load("document.xml");
 $xslt = new XSLTProcessor();
 $xslt->importStylesheet($xslDoc);
 echo $xslt->transformToXml($xmlDoc).PHP_EOL;
+
+// XML tree parsers: DOM and SimpleXML
+// XML Event-based parsers are faster and consume less than XML tree parser: XMLReader, XML Expat Parser
+
+// XML Constants (https://secure.php.net/manual/en/xml.constants.php)
+/*
+Prefix      Code                Description
+XML_ERROR_
+            SYNTAX
+            INVALID TOKEN
+            UNKNOWN_ENCODING
+XML_OPTION_
+            CASE_FOLDING        Enabled by default and sets element names to uppercase.
+            SKIP_WHITE          Skips excess whitespace in the source document
+*/
+
+// XML supported encoding:
+// UTF-8
+// ISO-8859-1 (unicode)
+// US-ASCII
